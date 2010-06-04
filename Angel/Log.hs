8558a3eb697897a607eceb262e8eb1080d1219f9
Jamie Turner <jamie@bu.mp>
2010-06-03 19:00:58 -0700

    not using PP lib

--- Angel/Log.hs
+++ Angel/Log.hs
@@ -1,17 +1,10 @@
 module Angel.Log where
 
 import Text.Printf (printf)
-import Text.PrettyPrint.HughesPJ
 import System.Time (getClockTime, toCalendarTime, CalendarTime(..), formatCalendarTime)
 import System.Locale (defaultTimeLocale)
 
 cleanCalendar ct = formatCalendarTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" ct
-
---makePpDoc ct = date 
---    where
---        date = (makeText $ ctYear ct) <> slash <> (makeText $ ctMonth ct) <> slash <> (makeText $ ctDay ct)
---        makeText = text . show
---        slash = char '/'
 
 logger lname msg = do tm <- getClockTime
                       ct <- toCalendarTime tm
