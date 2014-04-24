2e3d4b28100baa7c80b31a4b42288d6f151acf15
Michael Xavier <michael@michaelxavier.net>
2014-04-24 13:11:58 -0700

    fix failing test

--- test/Angel/PidFileSpec.hs
+++ test/Angel/PidFileSpec.hs
@@ -5,7 +5,6 @@ import Angel.PidFile
 
 import Control.Exception.Base ( try
                               , SomeException )
-import Control.Monad (void)
 import Data.Char (isNumber)
 import Data.IORef ( newIORef
                   , readIORef
@@ -16,10 +15,10 @@ import System.Posix.Files (fileExist)
 import SpecHelper
 
 spec :: Spec
-spec = do
+spec =
   describe "startWithPidFile" $ do
     it "creates the pidfile and cleans up" $ do
-      startWithPidFile procSpec fileName jogOn $ \pHandle -> do
+      startWithPidFile procSpec fileName jogOn $ \_pHandle -> do
         fileShouldExist fileName
         pid <- readFile fileName
         null pid `shouldBe` False
@@ -28,15 +27,15 @@ spec = do
     it "calls the error callback when pidfile can't be created and re-raises" $ do
       called <- newIORef False
       let onPidError = const $ writeIORef called True
-      (res :: Either SomeException ()) <- try $ startWithPidFile procSpec badPidFile onPidError jogOn
+      (res :: Either SomeException ()) <- try $ startWithPidFile procSpec badPidFile jogOn onPidError
       readIORef called `shouldReturn` True
       isLeft res `shouldBe` True
   where
     fileName = "temp.pid"
     badPidFile = "/bogus/path/to/pidfile"
     procSpec = proc "pwd" []
-    fileShouldExist name    = fileExist fileName `shouldReturn` True
-    fileShouldNotExist name = fileExist fileName `shouldReturn` False
+    fileShouldExist _name    = fileExist fileName `shouldReturn` True
+    fileShouldNotExist _name = fileExist fileName `shouldReturn` False
     jogOn = const $ return ()
     isLeft (Left _) = True
     isLeft _        = False
