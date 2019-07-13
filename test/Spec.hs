d48089637e39f9aef9445a75fc8611a300cccd6c
Tim Humphries <tim@utf8.me>
2019-07-13 15:28:16 -0700

    Remove custom Setup.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,15 +9,31 @@ import qualified Angel.UtilSpec
 -------------------------------------------------------------------------------
 import SpecHelper
 -------------------------------------------------------------------------------
+import Control.Exception (throwIO)
+import System.Exit (ExitCode (..))
+import System.Process (rawSystem)
 
 
+buildTestFixtures :: IO ()
+buildTestFixtures = do
+  run "ghc" ["--make", "test/test_jobs/StubbornJob.hs", "-o", "test/test_jobs/StubbornJob"]
+  run "ghc" ["--make", "test/test_jobs/CompliantJob.hs", "-o", "test/test_jobs/CompliantJob"]
+  where
+    run a b = do
+      e <- rawSystem a b
+      case e of
+        ExitSuccess ->
+          return ()
+        ExitFailure _ ->
+          throwIO e
 
 main :: IO ()
-main = defaultMain $ testGroup "angel"
-  [
-    Angel.ConfigSpec.spec
-  , Angel.JobSpec.spec
-  , Angel.LogSpec.spec
-  , Angel.PidFileSpec.spec
-  , Angel.UtilSpec.spec
-  ]
+main = do
+  buildTestFixtures
+  defaultMain $ testGroup "angel" [
+      Angel.ConfigSpec.spec
+    , Angel.JobSpec.spec
+    , Angel.LogSpec.spec
+    , Angel.PidFileSpec.spec
+    , Angel.UtilSpec.spec
+    ]
